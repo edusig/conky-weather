e76b6c9a01f2a8dcc57158ace3d610d49912a498
Eduardo Ciciliato <eduardo.ciciliato@gmail.com>
2015-12-26 16:40:58 -0200

    Adds script and gifs

--- weather.py
+++ weather.py
@@ -9,42 +9,48 @@ DEFAULT_UNIT_CELSIUS = True
 CACHE_TIME = 1800 # In seconds (1800 = 30 minutes)
 
 
-def extract_data(data, woeid):
+def extract_data(data, woeid, use_celsius):
+	if use_celsius:
+		unit = 'C'
+	else:
+		unit = 'F'
 	title = 'Weather for ' + data['location']['city'] + " - " + data['location']['region']
 	condition = data['item']['condition']
-	cond = ' Temp: ' + condition['temp'] + 'C \n ' + condition['text']
+	cond = ' Temp: ' + condition['temp'] + unit + ' \n ' + condition['text']
 	image = 'gif/' + condition['code'] + '.gif'
 	# Copy one of the condition gif images to the base path of the script to be used by conky
 	copyfile(os.path.join(base, image), os.path.join(base, 'condition_'+woeid+'.gif'))
 	return title, cond, image
 
 
-def weather(woeid, use_celsius):
+def weather(woeid, cache_time, use_celsius):
 
-	# Look for a cache file for your location
-	if os.path.isfile(os.path.join(base ,'cache_'+woeid+'.txt')):
-		with open(os.path.join(base, 'cache_'+woeid+'.txt'), 'r') as f:
+	celsius = ''
+	intcelsius = 0
+	if use_celsius:
+		intcelsius = 1
+		celsius = " and u='c'"
+	# Look for a cache file for your location and temperature type
+	if os.path.isfile(os.path.join(base ,'cache_'+woeid+'_'+str(intcelsius)+'.txt')):
+		with open(os.path.join(base, 'cache_'+woeid+'_'+str(intcelsius)+'.txt'), 'r') as f:
 			date, title, cond1, cond2, image = f.read().splitlines()
 			now = datetime.utcnow()
 			fdate = datetime.strptime(date[:19], '%Y-%m-%d %H:%M:%S')
 			tdelta = now - fdate
-			if tdelta.seconds < CACHE_TIME:
+			if tdelta.seconds < cache_time:
 				return title, cond1 + '\n' + cond2, image
 
 	# If there is no cache file or it expired, get from yahoo
 	baseurl = "https://query.yahooapis.com/v1/public/yql?"
 	# The temperature unit is set to celsius using "u='c'" in the query
-	celsius = ''
-	if use_celsius:
-		celsius = " and u='c'"
 	yql_query = "select * from weather.forecast where woeid="+woeid+celsius
 	yql_url = baseurl + urllib.urlencode({'q':yql_query}) + "&format=json"
 	result = urllib2.urlopen(yql_url).read()
 	data = json.loads(result)
-	formated = extract_data(data['query']['results']['channel'], woeid)
+	formated = extract_data(data['query']['results']['channel'], woeid, use_celsius)
 	
 	# Saves a new cache file for your location
-	with open(os.path.join(base, 'cache_'+woeid+'.txt'), 'w+') as f:
+	with open(os.path.join(base, 'cache_'+woeid+'_'+str(intcelsius)+'.txt'), 'w+') as f:
 		f.write(str(datetime.utcnow())[:19]+'\n')
 		f.write(formated[0]+'\n')
 		f.write(formated[1]+'\n')
@@ -54,14 +60,21 @@ def weather(woeid, use_celsius):
 
 if __name__ == '__main__':
 	location = str(DEFAULT_LOCATION)
+	cache = int(CACHE_TIME)
 	celsius = DEFAULT_UNIT_CELSIUS
 
 	if len(sys.argv) > 1:
 		location = sys.argv[1]
 	if len(sys.argv) > 2:
-		celsius = sys.argv[2]
+		cache = int(sys.argv[2])
+	if len(sys.argv) > 3:
+		celsius = int(sys.argv[3])
+		if celsius == 1:
+			celsius = True
+		else:
+			celsius = False
 
-	title, cond, image = weather(location, celsius)
+	title, cond, image = weather(location, cache, celsius)
 	print(title)
 	conds = cond.split('\n')
 	print
